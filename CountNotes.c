d7cb135dde4477bb6928b8dfa6bd7cc0ce84b5d1
AnjaliiBhardwaj <anjalibhardwaj823@gmail.com>
2023-01-27 00:47:16 +0530

    count no of notes

---
+++ CountNotes.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+int main(){
+    int amount;
+    int note500=0, note100=0, note50=0, note20=0, note10=0, note5=0, note2=0, note1=0;
+    printf("Enter the amount value: ");
+    scanf("%d",&amount);
+
+    if(amount >= 500)
+    {
+        note500 = amount/500;
+        amount -= (note500 * 500);
+    }
+    if(amount >= 100)
+    {
+        note100 = amount/100;
+        amount -= (note100 * 100);
+    }
+    if(amount >= 50)
+    {
+        note50 = amount/50;
+        amount -= (note50 * 50);
+    }
+    if(amount >= 20)
+    {
+        note20 = amount/20;
+        amount -=( note20 * 20);
+    }
+    if(amount >= 10)
+    {
+        note10 = amount/10;
+        amount -= (note10 * 10);
+    }
+    if(amount >= 5)
+    {
+        note5 = amount/5;
+        amount -= (note5 * 5);
+    }
+    if(amount >= 2)
+    {
+        note2 = amount /2;
+        amount -= (note2 * 2);
+    }
+    if(amount >= 1)
+    {
+        note1 = amount;
+    }
+
+    printf("Number of notes = \n");
+    fflush(stdin);
+    printf("500 = %d\n", note500);
+    fflush(stdin);
+    printf("100 = %d\n", note100);
+    fflush(stdin);
+    printf("50 = %d\n", note50);
+    fflush(stdin);
+    printf("20 = %d\n", note20);
+    fflush(stdin);
+    printf("10 = %d\n", note10);
+    fflush(stdin);
+    printf("5 = %d\n", note5);
+    fflush(stdin);
+    printf("2 = %d\n", note2);
+    fflush(stdin);
+    printf("1 = %d\n", note1);
+    fflush(stdin);
+
+    return 0;
+}
+
+    
