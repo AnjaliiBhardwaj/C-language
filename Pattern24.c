23ddf8f4391c45a39494fe93311464c8eb59a6be
AnjaliiBhardwaj <anjalibhardwaj823@gmail.com>
2023-02-19 20:45:54 +0530

    pattern

---
+++ Pattern24.c
@@ -0,0 +1,30 @@
+/*
+5
+    1
+   232
+  34543
+ 4567654
+567898765
+*/
+#include <stdio.h>
+int main(){
+    int rows;
+    scanf("%d",&rows);
+    
+    
+    for (int i=1;i<=rows;i++){
+        for(int s=1; s<=rows-i;s++){
+            printf(" ");
+        }
+        
+        for(int j=1; j<=i;j++){
+            printf("%d",i+j-1);
+            
+        }
+        for(int j=i-1;j>=1;j--){
+            printf("%d",i+j-1);
+        }
+        printf("\n");
+    }
+    
+}
